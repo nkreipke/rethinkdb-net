7c8a0fc0f6ee7a36c6bf040ff50e357b0a43f907
nkreipke <git@nkreipke.de>
2016-10-08 12:24:10 +0200

    Fix Assembly names and versions

--- rethinkdb-net-appconfig/Properties/AssemblyInfo.cs
+++ rethinkdb-net-appconfig/Properties/AssemblyInfo.cs
@@ -7,8 +7,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RethinkDB-Net.AppConfig")]
+[assembly: AssemblyProduct("RethinkDb.AppConfig")]
 [assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("0.13.0.0")]
+[assembly: AssemblyFileVersion("0.13.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
